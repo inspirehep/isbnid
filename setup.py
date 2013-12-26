fb4baeacb32875d23fa4eadab7782be2db18efd5
PDFnorm GitHub <pdfnorm@gmx.com>
2013-12-26 16:22:10 +0100

    disutils

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup, Extension
-
+from distutils.core import setup, Extension
 
 # digit = Extension('isbn.digit', sources = ['isbn/digitmodule.c'])
 # chyphen = Extension('isbn.chyphen', sources = ['isbn/chyphenmodule.c'])
 
 setup(name = 'isbnid',
-    version = '0.2.1',
+    version = '0.2.2',
     description = "Python ISBN ids",
     license = "LGPL",
     url = 'http://code.google.com/p/isbnid/',
@@ -21,9 +20,8 @@ setup(name = 'isbnid',
         "Topic :: Text Processing :: Indexing",
         ],
     packages = ['isbn'],
-    package_data = {'': ['isbn/data/*.xml']},
-    include_package_data=True,
-    # ext_modules = [digit, chyphen],
+    package_dir = {'isbn': 'isbn'},
+    package_data = {'isbn': ['data/*.xml']}
     )
     
 
